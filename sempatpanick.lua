e7ef397fdbc4e361e31eff8c15fc12d3db95634d
sempatpanick <muhdadangs@gmail.com>
2026-03-07 04:12:45 +0700

    refactor the main code to generalize the base url

--- sempatpanick.lua
+++ sempatpanick.lua
@@ -1,10 +1,12 @@
+local baseURL = "https://raw.githubusercontent.com/sempatpanick/roblox-script-experimental/refs/heads/main"
+
 local games = {
-    [83369512629707] = "http://localhost:5500/sawah_indo.lua",
-    [128070940451265] = "http://localhost:5500/speed_bike_escape.lua",
-    [2693023319] = "http://localhost:5500/expedition_antartica.lua",
+    [83369512629707] = baseURL .. "/sawah_indo.lua",
+    [128070940451265] = baseURL .. "/speed_bike_escape.lua",
+    [2693023319] = baseURL .. "/expedition_antartica.lua",
 }
 
-local fallbackScriptURL = "http://localhost:5500/others.lua"
+local fallbackScriptURL = baseURL .. "/others.lua"
 
 local currentID = game.PlaceId
 local scriptURL = games[currentID] or fallbackScriptURL
